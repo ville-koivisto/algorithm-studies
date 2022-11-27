ef8288102f7db9939845bebb52e1880ffaca6722
Ville Koivisto <ville.koivisto@solita.fi>
2022-11-27 21:46:56 +0200

    Prime calculator

---
+++ week01/count_primes.py
@@ -0,0 +1,17 @@
+"""
+A function for counting how many prime numbers there are from 2 to n.
+"""
+
+def count_primes (n):
+    primes = []
+    for i in range(2, n):
+        for j in primes:
+            if i%j == 0:
+                break
+        else:
+            primes.append(i)
+
+    return len(primes)
+
+if __name__ == "__main__":
+    print(count_primes(10)) # 4
